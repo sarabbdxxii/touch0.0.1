cb56fdb1058b83da3d6ea584c121ab6518d930bd
sarabbdxxii <hanabihanabi@126.com>
2017-08-10 16:31:14 +0800

    touch

---
+++ js/LAreaData1.js
@@ -0,0 +1,25 @@
+var LAreaData = [{
+    "id": "1",
+    "name": "beijig",
+    "child": [{
+        "id": "22",
+        "name": "\u4e1c\u57ce\u533a"
+    },
+    {
+        "id": "23",
+        "name": "\u5927\u5174\u533a"
+    }]
+
+},{
+    "id": "2",
+    "name": "\u5317\u4eac\u5e02",
+    "child": [{
+        "id": "24",
+        "name": "shanghai"
+    },
+    {
+        "id": "25",
+        "name": "\u5927\u5174\u533a"
+    }]
+
+}]
